d5eb9ef1d3af6a77746b3aede55fef0f7ffacc9a
Konstantin Baierer <unixprog@gmail.com>
2019-10-17 14:51:43 +0200

    wip

--- script.js
+++ script.js
@@ -7,19 +7,6 @@ window.app = new Vue({
   computed: {
     repos() {
       return this.repos_raw
-      // return this.repos_raw.map(repo_raw => {
-      //   console.log('xxx', {repo_raw})
-      //   const ret = {}
-      //   Object.entries(repo_raw).forEach(([k, v]) => {
-      //     console.log({k, v})
-      //     if (typeof v == Object) {
-      //       ret = {...ret, ...v}
-      //     } else {
-      //       ret[k] = v
-      //     }
-      //   })
-      //   return ret
-      // })
     }
   },
   mounted() {
@@ -27,7 +14,7 @@ window.app = new Vue({
   },
   methods: {
     fetchRepoData() {
-      fetch('/repos.json').then(resp => resp.json()).then(repos => this.repos.push(...repos))
+      fetch('repos.json').then(resp => resp.json()).then(repos => this.repos.push(...repos))
     },
   }
 })
