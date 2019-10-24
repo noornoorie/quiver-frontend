9f12e34a1f95c2b4806014099ef2c7565c486487
Konstantin Baierer <unixprog@gmail.com>
2019-10-24 13:02:29 +0200

    require ocrd >= 1.0.0

--- kwalitee/kwalitee.py
+++
@@ -1,8 +0,0 @@
-from ocrd_utils import pushd_popd
-def assess_kwalitee(repo):
-    ret = {}
-    # ocrd-tool.json
-    with open('ocrd-tool.json')
-    ret['ocrd-tool'] = 
-    # TODO has docker file?
-    print("TODO")
