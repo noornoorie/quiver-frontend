118c70454a593cf6815a1c8d7e02191c7c19062a
Paul Pestov <kontakt@paulpestov.de>
2022-09-27 22:38:35 +0200

    Set web hash history in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import Workflows from "@/components/Workflows.vue";
 import Projects from "@/components/Projects.vue";
 import Processors from "@/components/Processors.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
