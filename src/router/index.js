6b92f0f2a9e6e57c50e9ad12b3fbdfe343c9e00e
Paul Pestov <10750176+paulpestov@users.noreply.github.com>
2023-11-05 21:48:44 +0100

    fix: add route to timeline

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
-import Workflows from "@/components/Workflows.vue";
-import Projects from "@/components/Projects.vue";
-import Processors from "@/components/Processors.vue";
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Workflows from "@/components/Workflows.vue"
+import Projects from "@/components/Projects.vue"
+import Processors from "@/components/Processors.vue"
+import Timeline from "@/components/Timeline.vue"
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -15,9 +16,10 @@ const router = createRouter({
         { path: 'projects', name: 'projects', component: Projects },
         { path: 'processors', name: 'processors', component: Processors },
         { path: 'workflows', alias: '/', name: 'workflows', component: Workflows },
+        { path: 'timeline', alias: '/', name: 'timeline', component: Timeline },
       ],
     },
   ]
-});
+})
 
-export default router;
+export default router
