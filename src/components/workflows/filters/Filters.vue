b0e4b7c345491a0b8fe0e148318d6b14146fb3c8
Paul Pestov <10750176+paulpestov@users.noreply.github.com>
2023-12-05 22:33:19 +0100

    feat: add gt filter label

--- src/components/workflows/filters/Filters.vue
+++ src/components/workflows/filters/Filters.vue
@@ -12,21 +12,25 @@ onMounted(() => {
 </script>
 
 <template>
-  <MultiSelect
-    :model-value="filtersStore.gt"
-    @update:model-value="filtersStore.gt = $event"
-    :options="gtOptions"
-    optionLabel="label"
-    placeholder="Select Ground Truth"
-    panel-class="max-w-[500px]"
-    max-selected-labels="1"
-    filter
-    class="relative"
-  >
-    <template #option="{ option }: { option: FilterOption }">
-      <span class="truncate">{{ option.label }}</span>
-    </template>
-  </MultiSelect>
+  <div class="flex items-center">
+    <span class="font-semibold mr-4">{{ $t('documents')}} ({{ $t('ground_truth')}}):</span>
+    <MultiSelect
+        :model-value="filtersStore.gt"
+        @update:model-value="filtersStore.gt = $event"
+        :options="gtOptions"
+        optionLabel="label"
+        placeholder="Select Ground Truth"
+        panel-class="max-w-[500px]"
+        max-selected-labels="1"
+        filter
+        class="relative flex-1"
+    >
+      <template #option="{ option }: { option: FilterOption }">
+        <span class="truncate">{{ option.label }}</span>
+      </template>
+    </MultiSelect>
+  </div>
+
 </template>
 
 <style scoped lang="scss">
